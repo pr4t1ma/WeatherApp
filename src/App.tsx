bd5d7a0fbf46bbc4b37800ddd10cb8a5278f3d4a
Pam <prati.ma@hotmail.com>
2024-10-27 13:17:25 +0100

    add items for the menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import "./App.css";
+const items = [
+  { label: "Home", href: "./" },
+  { label: "About", href: "./about" },
+  { label: "Services", href: "./services" },
+  { label: "Products", href: "./products" },
+  { label: "Blog", href: "./blog" },
+  { label: "Careers", href: "./careers" },
+  { label: "Contact", href: "./contact" },
+];
 
 function App() {
   return (
     <>
       <h1>Weather App</h1>
       <img src="" alt="" />
+      <ul>
+        <li>
+          <a href="">Home</a>
+        </li>
+      </ul>
     </>
   );
 }
